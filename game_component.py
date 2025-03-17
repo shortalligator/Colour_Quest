f20e53460b4b107b51862382995e7b96d4f140ed
shortalligator <101073123+shortalligator@users.noreply.github.com>
2025-03-17 14:59:30 +1300

    Add files via upload

---
+++ game_component.py
@@ -0,0 +1,68 @@
+from tkinter import *
+from functools import partial # to prevent unwanted windows
+
+
+class StartGame:
+    """
+    Initial Game interface (asks users how many rounds they
+    want to play)
+    """
+
+    def __init__(self):
+        """
+        Gets number of rounds from user
+        """
+
+        self.start_frame = Frame(padx=10, pady=10)
+        self.start_frame.grid()
+
+        # create play button
+        self.play_button = Button(self.start_frame, font=("Arial", "16", "bold"),
+                                  fg="#FFFFFF", bg="#0057D8", text="Play",
+                                  width=10, command=self.check_rounds)
+        self.play_button.grid(row=0, column=1)
+
+    def check_rounds(self):
+        Play(5)
+        root.withdraw()
+class Play:
+    """
+    Interface for playing the Colour Quest game
+    """
+
+    def __init__(self, how_many):
+        self.play_box = Toplevel()
+
+        self.game_frame = Frame(self.play_box)
+        self.game_frame.grid(padx=10, pady=10)
+
+        # body font for most labels
+        body_font = ("Arial", "12")
+
+        # List for label details (text | font | background | row)
+        play_labels_list = [
+            ["Round # of #", ("Arial", "16", "bold"), None, 0],
+            ["Score to beat: #", body_font, "#FFF2CC", 1],
+            ["Choose a colour below. Good Luck. 🍀", body_font, "#D5E8D4", 2],
+            ["You chose, ______", body_font, "D5E8D4", 4]
+        ]
+
+        play_labels_ref = []
+        for item in play_labels_list:
+            self.make_label = Label(self.game_frame, text=item[0], font=item[1],
+                                    bg=item[2], wraplength=300, justify="left")
+            self.make_label.grid(row=item[3], padx=10, pady=10)
+
+    def close_play(self):
+        # reshow root (ie: choose rounds) and end current
+        # game / allow new game to start
+        root.deiconify()
+        self.play_box.destroy()
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Colour Quest")
+    StartGame()
+    root.mainloop()
