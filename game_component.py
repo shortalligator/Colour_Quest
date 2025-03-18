293dad05cf0300193b57d003c061037075c82fa3
shortalligator <101073123+shortalligator@users.noreply.github.com>
2025-03-19 10:04:13 +1300

    Add files via upload

--- game_component.py
+++ game_component.py
@@ -44,7 +44,7 @@ class Play:
             ["Round # of #", ("Arial", "16", "bold"), None, 0],
             ["Score to beat: #", body_font, "#FFF2CC", 1],
             ["Choose a colour below. Good Luck. 🍀", body_font, "#D5E8D4", 2],
-            ["You chose, ______", body_font, "D5E8D4", 4]
+            ["You chose, ______", body_font, "#D5E8D4", 4]
         ]
 
         play_labels_ref = []
@@ -52,6 +52,36 @@ class Play:
             self.make_label = Label(self.game_frame, text=item[0], font=item[1],
                                     bg=item[2], wraplength=300, justify="left")
             self.make_label.grid(row=item[3], padx=10, pady=10)
+
+            play_labels_ref.append(item)
+
+        # retrieve labels so they can be configured later
+        self.heading_label = play_labels_ref[0]
+        self.target_label = play_labels_ref[1]
+        self.results_label = play_labels_ref[3]
+
+        # set up colour buttons
+        self.colour_frame = Frame(self.game_frame)
+        self.colour_frame.grid(row=3)
+
+        # create 4 buttons in a  2 x 2 grid
+        for item in range(0, 4):
+            self.colour_button = Button(self.colour_frame, font=("Arial", "12"),
+                                        text="Colour Name", width=15)
+            self.colour_button.grid(row=item // 2,
+                                    column=item % 2, padx=5, pady=5)
+
+        # frame to hold hints and stats button
+        self.hints_stats_frame = Frame(self.game_frame)
+        self.hints_stats_frame.grid(row=6)
+
+        # list for buttons (frame | text | bg | command | width | row | column)
+        control_buttons_list = [
+            [self.game_frame, "Next Round", "#0057D8", "", 21, 5, None],
+            [self.hints_stats_frame, "Hints", "#FF8000", "", 10, 0, 0],
+            [self.hints_stats_frame, "Stats", "#333333", "", 10, 0, 1],
+            [self.game_frame, "End", "#990000", self.close_play, 30, 7, None]
+        ]
 
     def close_play(self):
         # reshow root (ie: choose rounds) and end current
