5777b34e54fa6d0a755ded89715f68cb3d6e8ed6
shortalligator <101073123+shortalligator@users.noreply.github.com>
2025-03-28 10:57:22 +1300

    Add files via upload

--- 01_Colour_quest.py
+++ 01_Colour_quest.py
@@ -70,15 +70,86 @@ class StartGame:
         self.start_frame = Frame(padx=10, pady=10)
         self.start_frame.grid()
 
+        # strings for labels
+        intro_string = "In each round you will be invited to choose a colour. Your goal is " \
+                       "to beat the target score and win the round (and keep your points)."
+
+        choose_string = "How many rounds do you want to play?"
+
+        # List of labels to be made (text | font | fg)
+        start_labels_list = [
+            ["Colour Quest", ("Arial", "16", "bold"), None],
+            [intro_string, ("Arial", "12"), None],
+            [choose_string, ("Arial", "12", "bold"), "#17b317"]
+        ]
+
+        # Create labels and add them to the reference list...
+
+        start_labels_ref = []
+        for count, item in enumerate(start_labels_list):
+            make_label = Label(self.start_frame, text=item[0], font=item[1],
+                               fg=item[2],
+                               wraplength=350, justify="left", pady=10, padx=20)
+            make_label.grid(row=count)
+
+            start_labels_ref.append(make_label)
+
+        # extract choice label so that it can be changed into an error message
+        self.choose_label = start_labels_ref[2]
+
+        # frame so that entry box and bottom can be in the same row
+        self.entry_area_frame = Frame(self.start_frame)
+        self.entry_area_frame.grid(row=3)
+
+        self.num_rounds_entry = Entry(self.entry_area_frame,
+                                      font=("Arial", "20", "bold",),
+                                      width=10)
+        self.num_rounds_entry.grid(row=0, column=0, padx=10, pady=10)
+
         # create play button
-        self.play_button = Button(self.start_frame, font=("Arial", "16", "bold"),
+        self.play_button = Button(self.entry_area_frame, font=("Arial", "16", "bold"),
                                   fg="#FFFFFF", bg="#0057D8", text="Play",
                                   width=10, command=self.check_rounds)
         self.play_button.grid(row=0, column=1)
 
     def check_rounds(self):
-        Play(5)
-        root.withdraw()
+        """
+        Checks that users have entered 1 or more rounds
+        """
+        # Retrieve temperature to be converted
+        rounds_wanted = self.num_rounds_entry.get()
+
+        # reset label and entry box (for when users come back to home screen)
+        self.choose_label.config(fg="#17b317", font=("Arial", "12", "bold"))
+        self.num_rounds_entry.config(bg="#FFFFFF")
+
+        error = "Oops - please choose a whole number more than zero."
+        has_errors = "no"
+
+        # checks that the amount wanted is a number above absolute zero
+        try:
+            rounds_wanted = int(rounds_wanted)
+            if rounds_wanted > 0:
+                # Clear entry box and reset instruction label so that
+                # when users play a new game, they don't see an error message
+                self.num_rounds_entry.delete(0, END)
+                self.choose_label.config(text="How many rounds do you want to play?")
+
+                # invoke play class (and take across number of rounds)
+                Play(rounds_wanted)
+                # invoke root window (ie: hide rounds choice window)
+                root.withdraw()
+            else:
+                has_errors = "yes"
+        except ValueError:
+            has_errors = "yes"
+
+        # display the error if necessary
+        if has_errors == "yes":
+            self.choose_label.config(text=error, fg="#990000",
+                                     font=("Arial", "10", "bold"))
+            self.num_rounds_entry.config(bg="#F4CCCC")
+            self.num_rounds_entry.delete(0, END)
 
 
 class Play:
